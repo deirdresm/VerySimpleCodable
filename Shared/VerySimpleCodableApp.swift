9f91eab29522021530c748b5f95a9c949b67448d
Deirdre Saoirse Moen <deirdre@deirdre.net>
2021-12-23 12:51:32 -0800

    Initial Commit

---
+++ Shared/VerySimpleCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerySimpleCodableApp.swift
+//  Shared
+//
+//  Created by Deirdre Saoirse Moen on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct VerySimpleCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
